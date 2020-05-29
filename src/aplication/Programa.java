aaa270a5206ffc4b34ea30f276fe022fa378e87d
CristianPassos <cristianpassosdasilva@gmail.com>
2020-05-29 19:47:29 -0300

    Leitura dos dados

--- src/aplication/Programa.java
+++ src/aplication/Programa.java
@@ -1,9 +1,35 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        String funcionario;
+        char digitOutro;
+        double vlrHora;
+        int horasTrab;
+        
+        System.out.println("Nome: ");
+        funcionario = sc.nextLine();
+        System.out.println("Horas trabalhadas: ");
+        horasTrab = sc.nextInt();
+        System.out.println("Valor por hora: ");
+        vlrHora = sc.nextDouble();
+        
+        while (horasTrab < 0) {
+        	System.out.println("A hora trabalhada não deve ser negativa. Tente novamente: ");
+        	horasTrab = sc.nextInt();			
+		}
+        while (vlrHora < 0) {
+        	System.out.println("O valor da hora não deve ser negativa. Tente novamente: ");
+        	vlrHora = sc.nextDouble();			
+		}
+		sc.close();
 
 	}
 
